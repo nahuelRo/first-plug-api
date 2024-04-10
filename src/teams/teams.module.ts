3fdb5a29029b720ff0909df74c95cfe8e669ee8e
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-04-10 19:01:49 -0300

    fix: add middleware in teams module

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { TeamsController } from './teams.controller';
 import { tenantModels } from '../common/providers/tenant-models-provider';
 import { TenantsModule } from 'src/tenants/tenants.module';
+import { TenantsMiddleware } from 'src/common/middlewares/tenants.middleware';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TenantsModule],
   controllers: [TeamsController],
-  providers: [TeamsService, tenantModels.memberModel],
+  providers: [TeamsService, tenantModels.memberModel, JwtService],
 })
-export class TeamsModule {}
+export class TeamsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TenantsMiddleware).forRoutes(TeamsController);
+  }
+}
