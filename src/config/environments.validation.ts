101effcc1ddf3335ac507a83333bac74252cb0db
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-05-12 13:01:14 -0300

    chore: change joi by zod

---
+++ src/config/environments.validation.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const ZodEnvironmentsSchema = z.object({
+  DB_CONNECTION_STRING: z.string().min(1),
+  PORT: z.string().default('3001'),
+  JWTSECRETKEY: z.string().min(1),
+  JWTREFRESHTOKENKEY: z.string().min(1),
+});
