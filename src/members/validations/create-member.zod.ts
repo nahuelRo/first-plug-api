c33e4e16e685f49b766362810e7bc1c3f6ec19b0
MercedesPoisson <mercedespoisson@yahoo.com>
2024-06-03 09:29:27 -0300

    members zod schema, add validations

--- src/members/validations/create-member.zod.ts
+++ src/members/validations/create-member.zod.ts
@@ -3,13 +3,24 @@ import { ProductSchemaZod } from 'src/products/validations/create-product.zod';
 import validator from 'validator';
 
 export const MemberSchemaZod = z.object({
-  firstName: z.string().min(1).trim(),
-  lastName: z.string().min(1).trim(),
-  email: z.string().email().trim().toLowerCase(),
-  picture: z.instanceof(File).optional(),
+  firstName: z.string().min(1, { message: 'First name is required' }).trim(),
+  lastName: z.string().min(1, { message: 'Last name is required' }).trim(),
+  email: z
+    .string()
+    .email({ message: 'Invalid email address' })
+    .trim()
+    .toLowerCase()
+    .min(1, { message: 'Email is required' }),
+  picture: z.string().optional(),
   position: z.string().trim().optional(),
   personalEmail: z.string().email().trim().toLowerCase().optional(),
-  phone: z.string().trim().refine(validator.isMobilePhone),
+  phone: z
+    .string()
+    .trim()
+    .refine(validator.isMobilePhone, {
+      message: 'Phone number is invalid',
+    })
+    .optional(),
   city: z.string().trim().optional(),
   // Contry list
   country: z.string().trim().optional(),
@@ -17,7 +28,13 @@ export const MemberSchemaZod = z.object({
   address: z.string().trim().optional(),
   apartment: z.string().trim().optional(),
   additionalInfo: z.string().trim().optional(),
-  startDate: z.string().trim().refine(validator.isISO8601).optional(),
+  startDate: z
+    .string()
+    .trim()
+    .refine(validator.isISO8601, {
+      message: 'Start date must be a valid ISO 8601 date',
+    })
+    .optional(),
   birthDate: z.string().trim().refine(validator.isISO8601).optional(),
   products: z.array(ProductSchemaZod).optional(),
   team: z.string().trim().optional(),
