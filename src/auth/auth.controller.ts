96d6b8a94ce732f69b7bcf0c6eefa8b5f750eb39
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-07-01 11:13:28 -0300

    refactor user info data enpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateTenantDto } from '../tenants/dto';
 import { TenantsService } from '../tenants/tenants.service';
@@ -16,6 +16,10 @@ export class AuthController {
     private readonly tenantService: TenantsService,
   ) {}
 
+  @Post('user')
+  async getUserIngo(@Body() { id }: { id: string }) {
+    return await this.tenantService.getTenantById(id);
+  }
   @Post('register')
   async registerUser(@Body() createTenantDto: CreateTenantDto) {
     return await this.tenantService.create(createTenantDto);
